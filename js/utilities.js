bb4539d5883ced38bd711dab8c5119a84f7fcdf9
mayeda21 <mkadhora@gmail.com>
2022-10-02 23:05:52 +0600

    validation for positive input number

--- js/utilities.js
+++ js/utilities.js
@@ -23,23 +23,35 @@ function selectButton(buttonId) {
 }
 // calculate per player expense
 document.getElementById("calculate").addEventListener("click", function () {
+   const playerExpense = document.getElementById("player-expense");
+   const playerContainer = document.getElementsByClassName("li");
    const perPlayerById = document.getElementById("per-player");
    const perPlayerString = perPlayerById.value;
    const perPlayerValue = parseInt(perPlayerString);
-   const playerExpense = document.getElementById("player-expense");
-   const playerContainer = document.getElementsByClassName("li");
-   playerExpense.innerText = perPlayerValue * playerContainer.length;
+   const numberValue = validationForNegativeNumber(perPlayerValue);
+   if (numberValue === true) {
+      playerExpense.innerText = perPlayerValue * playerContainer.length;
+   } else {
+      alert("put a positive value");
+      playerExpense.innerText = "00";
+   }
 });
 // get the all the manager and coach expenses and calculate total
 document
    .getElementById("calculate-total")
    .addEventListener("click", function () {
       const playerExpense = document.getElementById("player-expense").innerText;
+      const displayTotalById = document.getElementById("displayTotal");
       const managerValue = calculateTotal("manager");
       const coachValue = calculateTotal("coach");
-      const totalValue = parseInt(playerExpense) + managerValue + coachValue;
-      const displayTotalById = document.getElementById("displayTotal");
-      displayTotalById.innerText = totalValue;
+      // check the number of manager and coach and give a alert for negative value
+      if (managerValue < 0 || coachValue < 0) {
+         alert("put a positive value");
+         displayTotalById.innerText = "00";
+      } else {
+         const totalValue = parseInt(playerExpense) + managerValue + coachValue;
+         displayTotalById.innerText = totalValue;
+      }
    });
 // common funtion of get the value of manager and coach
 function calculateTotal(inputId) {
@@ -47,4 +59,12 @@ function calculateTotal(inputId) {
    const text = getTextValueById.value;
    const textValue = parseInt(text);
    return textValue;
+}
+// validation for negative number:
+function validationForNegativeNumber(number) {
+   if (number < 0) {
+      return false;
+   } else {
+      return true;
+   }
 }
