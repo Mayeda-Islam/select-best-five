086e609cc0152d17cb7e846051e3681c94b3662a
mayeda21 <mkadhora@gmail.com>
2022-10-02 22:31:18 +0600

    fav icon added

--- js/utilities.js
+++ js/utilities.js
@@ -1,11 +1,9 @@
 function selectButton(buttonId) {
-   console.log(buttonId);
    // set the player name into selected box
    const playerName = document.getElementById(
       buttonId + "-player-name"
    ).innerText;
    const playerContainer = document.getElementById("player-container");
-
    // disable button
    document.getElementById(buttonId).disabled = true;
    document
@@ -13,27 +11,26 @@ function selectButton(buttonId) {
       .classList.replace("hover:bg-blue-700", "disabled:opacity-50");
    let player = document.getElementsByClassName("li");
    const playerLength = player.length;
-   console.log(playerLength);
+   // count the player number and give a warning
    if (playerLength < 5) {
       const li = document.createElement("li");
       li.classList.add("li");
       li.innerText = playerName;
       playerContainer.appendChild(li);
    } else {
-      alert("You have selected the best five players");
+      alert("You can't select more than five players");
    }
 }
-
+// calculate per player expense
 document.getElementById("calculate").addEventListener("click", function () {
    const perPlayerById = document.getElementById("per-player");
    const perPlayerString = perPlayerById.value;
    const perPlayerValue = parseInt(perPlayerString);
-   console.log(perPlayerValue);
    const playerExpense = document.getElementById("player-expense");
    const playerContainer = document.getElementsByClassName("li");
-
    playerExpense.innerText = perPlayerValue * playerContainer.length;
 });
+// get the all the manager and coach expenses and calculate total
 document
    .getElementById("calculate-total")
    .addEventListener("click", function () {
@@ -43,11 +40,11 @@ document
       const totalValue = parseInt(playerExpense) + managerValue + coachValue;
       const displayTotalById = document.getElementById("displayTotal");
       displayTotalById.innerText = totalValue;
-      console.log(totalValue);
    });
+// common funtion of get the value of manager and coach
 function calculateTotal(inputId) {
-   const getManagerById = document.getElementById(inputId);
-   const managerString = getManagerById.value;
-   const managerValue = parseInt(managerString);
-   return managerValue;
+   const getTextValueById = document.getElementById(inputId);
+   const text = getTextValueById.value;
+   const textValue = parseInt(text);
+   return textValue;
 }
